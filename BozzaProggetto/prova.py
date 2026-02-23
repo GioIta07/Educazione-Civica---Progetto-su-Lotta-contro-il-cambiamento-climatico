3221a517e4e4bd910bcffb168d72bfbd9c6483e9
Giorgio_Italiano <Osuidiot.07.a@gmail.com>
2026-02-23 21:39:16 +0100

    Aggiunto il bottone "economia"

--- BozzaProggetto/prova.py
+++ BozzaProggetto/prova.py
@@ -1,4 +1,4 @@
 import webview, os
 script_dir = os.path.dirname(os.path.abspath(__file__))
-webview.create_window("Gioco", os.path.join(script_dir, "index.html"))
+webview.create_window("Introduzione", os.path.join(script_dir, "index.html"))
 webview.start()
