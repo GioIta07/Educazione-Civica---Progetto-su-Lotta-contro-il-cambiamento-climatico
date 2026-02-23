43dc339a67c61235bf4859ace933ff94982820fe
Giorgio_Italiano <Osuidiot.07.a@gmail.com>
2026-02-23 21:42:39 +0100

    Cambiato nomi per lettura più semplice

--- BozzaProggetto/gioco.py
+++ BozzaProggetto/gioco.py
@@ -1,4 +1,4 @@
 import webview, os
 script_dir = os.path.dirname(os.path.abspath(__file__))
-webview.create_window("Gioco", os.path.join(script_dir, "prova.html"))
+webview.create_window("Gioco", os.path.join(script_dir, "gioco.html"))
 webview.start()
